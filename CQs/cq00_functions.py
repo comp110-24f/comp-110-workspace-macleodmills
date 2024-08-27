9fde5b8fcc2cb3b45800e588fc79513a48dab364
MacLeod Mills <macleodm@unc.edu>
2024-08-27 14:37:55 -0400

    finished cq00

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,16 @@
+"""Practice with Functions"""
+
+__author__ = "730669747"
+
+"""Practice With Mimic"""
+
+
+def mimic(message: str) -> str:
+    return message
+
+
+print(mimic(message=input("What is your message?")))
+
+
+if __name__ == "__main__":
+    main()
