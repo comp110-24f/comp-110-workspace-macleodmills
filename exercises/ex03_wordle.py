91fe3a162cd07e2b4e74f42951379d0cde3c25c7
MacLeod Mills <macleodm@unc.edu>
2024-10-14 11:44:47 -0400

    new changes

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -67,4 +67,4 @@ def main(secret: str) -> None:
 
 
 if __name__ == "__main__":
-    main(secret="codes")  # codes is the word they are trying to guess
+    main(secret="penis")  # codes is the word they are trying to guess
--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -4,6 +4,8 @@ __author__ = "730669747"
 
 
 def all(input: list[int], target: int) -> bool:
+    """Defining all -should return a bool indicating whether or not all the ints in the
+    list are the same as the given int"""
     if len(input) == 0:
         return False
     for number in input:
@@ -13,10 +15,13 @@ def all(input: list[int], target: int) -> bool:
 
 
 def max(input: list[int]) -> int:
+    """Defining max - max should return the largest in the List."""
     if len(input) == 0:
-        raise ValueError("max() arg is an empty List")
+        raise ValueError(
+            "max() arg is an empty List"
+        )  # If list is empty return ValueError
 
-    current_max = input[0]
+    current_max = input[0]  # Setting current max to the first element at first
     for number in input:
         if number > current_max:
             current_max = number
@@ -24,21 +29,26 @@ def max(input: list[int]) -> int:
 
 
 def is_equal(a: list[int], b: list[int]):
+    """return True if every element at every index is equal in both lists."""
     if len(a) != len(b):
-        return False
+        return False  # if the lists dont have same indexes it will return false
 
     for i in range(len(a)):
         if a[i] != b[i]:
-            return False
-    return True
+            return False  #
+    return True  # if none of the if statements are entered than it will return true
 
 
 def extend(a: list[int], b: list[int]) -> None:
+    """Defining eextend - mutate the first list of int values by appending the second
+    lists values to the end of it."""
     for elem in b:
         a.append(elem)
+    return None
 
 
 if __name__ == "__main__":
+    """Testing the functions to make sure they all work"""
     # Testing the `all()` function
     print(all([1, 2, 3], 1))  # Should print: False
     print(all([1, 1, 1], 1))  # Should print: True
